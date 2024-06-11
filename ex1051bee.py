4660c960bcfd11be2d18a2fe5d4e44c1649fb542
Luis Paulo <91858026+LuisYnwa@users.noreply.github.com>
2024-06-11 13:00:27 -0300

    correcao

--- ex1051bee.py
+++ ex1051bee.py
@@ -8,9 +8,9 @@ elif salario > 2000.00 and salario <= 3000.00:
     print(f'R$ {calculo1:.2f}')
 
 elif salario > 3000.00 and salario <=4500.00:
-    calculo1 = (salario - 2000.00) * 0.18
-    calculo2 = 0.08 * 1000
-    print(f'R$ {calculo1 + calculo2:.2f}')
+    calculo01 = 0.08 * 1000
+    calculo02 = 0.18 * (salario - 3000)
+    print(f'R$ {calculo01 + calculo02:.2f}')
 
 else:
     calculo01 = 0.08 * 1000
